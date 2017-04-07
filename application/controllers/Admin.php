8221508cfddfe66339eccf645fb229fa1bb84688
Fly3949 <394976586@qq.com>
2017-04-07 15:20:08 +0800

    Update Admin.php
    Fix a bug

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
+class admin extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
